cf73e9bcd2fb2b920e6654c93a09efe971bb44e4
Muhammad Ahmad <ahmad.amjad09@gmail.com>
2020-06-23 21:31:38 +0500

    Received Ip from Web and Generate Graph for Traceroute Command

---
+++ app/routes.py
@@ -0,0 +1,20 @@
+from  app import app
+from flask import render_template, url_for, request
+from TraceRoute import Traceroute
+from IPGraph import Ipgraph
+trace = Traceroute()
+graph = Ipgraph()
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template('home.html',title='Home')
+@app.route('/traceIP',methods=['POST','GET'])
+def traceIP():
+
+       if request.method == 'POST':
+           ip = request.form['ip']
+           trace.findIps("facebook.com")
+           ip_list = trace.getIpList()
+           print(ip_list)
+           graph.set_Graph(ip_list)
+       return "Command Submit Successfully"
--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-from TraceRoute import Traceroute
-from IPGraph import Ipgraph
-trace = Traceroute()
-graph = Ipgraph()
-trace.findIps("google.com")
-ip_list = trace.getIpList()
-print (ip_list)
-graph.set_Graph(ip_list)
+from app import app
 
 
 
