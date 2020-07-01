8fc06c5aa0422dfd707155ac0c0a301a4c9661ef
Muhammad Ahmad <ahmad.amjad09@gmail.com>
2020-07-01 22:40:13 +0500

    change in IpGraph file

--- IPGraph.py
+++ IPGraph.py
@@ -1,5 +1,6 @@
 import networkx as nx
 import matplotlib.pyplot as plt
+import os
 class Ipgraph:
     def set_Graph(self,ips_list):
         length = len(ips_list)
@@ -8,7 +9,9 @@ class Ipgraph:
             G.add_edge(ips_list[i], ips_list[i + 1])
         nx.draw(G, with_labels=True)
         #plt.show()
-        plt.savefig('/home/ahmad/NMT/app/static/images/Graph')
+        dirname = os.path.dirname(__file__)
+        filename = os.path.join(dirname, 'app/static/images/Graph')
+        plt.savefig(filename)
 
 
 
